26a20e5b9227a4858582a4cd76df91f3d4e44067
Prajeen Govardhanam <p@prajeen.com>
2024-09-07 17:48:37 +0000

    :construction: Add loading placeholder for flags
    Signed-off-by: Prajeen Govardhanam <p@prajeen.com>

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -2,6 +2,22 @@ import Image from "next/image";
 
 import styles from "./CountryCard.module.css";
 
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) => Buffer.from(str).toString("base64");
+
 export default function CountryCard({ name, capitals, flag }: {
     name: string,
     capitals?: string,
@@ -9,7 +25,11 @@ export default function CountryCard({ name, capitals, flag }: {
 }) {
     return (
         <div className={styles.card}>
-            <Image className={styles.flag} src={flag} alt={`${name}'s flag`} width={300} height={200} />
+            <Image className={styles.flag}
+                src={flag} alt={`${name}'s flag`}
+                width={300}
+                height={200}
+                placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(300, 200))}`} />
             <h3 className={styles.title}>{name}</h3>
             {capitals && <p className={styles.subtext}>{capitals}</p>}
         </div>
