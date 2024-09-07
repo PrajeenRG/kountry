d6cd8114616506993b6f45a40876231615e18ef3
Prajeen Govardhanam <p@prajeen.com>
2024-09-07 17:35:57 +0000

    :construction: Add vercel analytics and speed insights
    Signed-off-by: Prajeen Govardhanam <p@prajeen.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const jost = Jost({
@@ -24,6 +26,8 @@ export default function RootLayout({
         className={`${jost.className} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
